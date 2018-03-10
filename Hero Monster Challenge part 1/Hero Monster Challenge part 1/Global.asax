a48306a9bf60ba4e92c5fbaf2d742f46e5fcac95
ThunderDart <caalexmartin@gmail.com>
2018-03-10 07:45:50 -0800

    Replaced Hero Monster challenge 1

---
+++ Hero Monster Challenge part 1/Hero Monster Challenge part 1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hero_Monster_Challenge_part_1.Global" Language="C#" %>
---
+++ Hero Monster Challenge part 1/Hero Monster Challenge part 1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hero_Monster_Challenge_part_1.Startup))]
+namespace Hero_Monster_Challenge_part_1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
