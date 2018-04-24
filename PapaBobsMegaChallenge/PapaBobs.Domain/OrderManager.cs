9b3c19056f334390e2f1e7dad8e04eae2b318500
Alex Martin <caalexmartin@gmail.com>
2018-04-23 21:54:36 -0700

    Final Dev U C# Challenge

---
+++ PapaBobsMegaChallenge/PapaBobs.Domain/OrderManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Domain
+{
+    public class OrderManager
+    {
+        public static void CreateOrder(DTO.OrderDTO orderDTO)
+        {
+
+            if (orderDTO.Name.Trim().Length == 0)
+                throw new Exception("Name is Required");
+
+            if (orderDTO.Address.Trim().Length == 0)
+                throw new Exception("address is Required");
+
+            if (orderDTO.Zip.Trim().Length == 0)
+                throw new Exception("Zip Code is Required");
+
+            if (orderDTO.Phone.Trim().Length == 0)
+                throw new Exception("Phone Number is Required");
+
+            orderDTO.OrderId = Guid.NewGuid();
+            orderDTO.TotalCost = PizzaPriceManager.CalculatePizzaCost(orderDTO);
+            Persistence.OrderRepository.CreateOrder(orderDTO);
+        }
+
+        public static object GetOrders()
+        {
+            return Persistence.OrderRepository.GetOrders();
+        }
+
+        public static void CompleteOrder(Guid orderId)
+        {
+            Persistence.OrderRepository.CompleteOrder(orderId);
+
+
+        }
+    }
+}
---
+++ PapaBobsMegaChallenge/PapaBobs.Domain/PizzaPriceManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PapaBobs.DTO;
+
+namespace PapaBobs.Domain
+{
+    public class PizzaPriceManager
+    {
+        public static decimal CalculatePizzaCost(DTO.OrderDTO order)
+        {
+            decimal cost = 0.0M;
+            var prices = getPizzaPrices();
+            cost += calculateSizeCost(order, prices);
+            cost += calcualteCrustCost(order, prices);
+            cost += calculateToppings(order, prices);
+
+
+            return cost;
+        }
+
+        private static decimal calculateToppings(DTO.OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+
+            cost += (order.Sausage) ? prices.Sausage : 0M;
+            cost += (order.Pepperoni) ? prices.Pepperoni : 0M;
+            cost += (order.Onions) ? prices.OnionsCost : 0M;
+            cost += (order.GreenPeppers) ? prices.GreenPeppersCost: 0M;
+
+            return cost;
+               
+        }
+
+        private static decimal calcualteCrustCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+
+            switch (order.Crust)
+            {
+                case DTO.Enums.CrustType.Regular:
+                    cost = prices.RegularCrustCost;
+                    break;
+                case DTO.Enums.CrustType.Thin:
+                    cost = prices.ThinCrustCost;
+                    break;
+                case DTO.Enums.CrustType.Thick:
+                    cost = prices.ThickCrustCost;
+                    break;
+                default:
+                    break;
+
+            }
+            return cost;
+        }
+
+        private static decimal calculateSizeCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+           decimal cost = 0.0M;
+
+            switch (order.Size)
+            {
+                case DTO.Enums.SizeType.Small:
+                    cost = prices.SmallSizeCost;
+                    break;
+                case DTO.Enums.SizeType.Medium:
+                    cost = prices.MediumSizeCost;
+                    break;
+                case DTO.Enums.SizeType.Large:
+                    cost = prices.LargeSizeCost;
+                    break;
+                default:
+                    break;
+            }
+
+            return cost;
+        }
+
+        private static DTO.PizzaPriceDTO getPizzaPrices()
+        {
+            var prices = Persistence.PizzaPriceRepository.GetPizzaPrices();
+            return prices;
+        }
+    }
+}
